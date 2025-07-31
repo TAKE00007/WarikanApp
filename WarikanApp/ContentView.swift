09af0e2877c96652847e04aa4c3350904d1d35a7
Otake Shun <131763561+TAKE00007@users.noreply.github.com>
2025-07-31 22:29:58 +0900

    Revert "グループ作成画面で名前を表示できるようにする（仮完成)"

---
+++ WarikanApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WarikanApp
+//
+//  Created by 大竹駿 on 2025/07/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("このアプリは割り勘を便利にするアプリです。")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
