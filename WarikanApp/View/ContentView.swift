d8f0ad0e976ea6bf1e6b31a7e1e30ff3457e22b9
TAKE00007 <xshun1590@icloud.com>
2025-09-09 21:48:49 +0900

    デザインを整えた

--- WarikanApp/View/ContentView.swift
+++ WarikanApp/View/ContentView.swift
@@ -14,17 +14,18 @@ struct ContentView: View {
             ZStack {
                 Color("main")
                     .edgesIgnoringSafeArea(.all)
-                VStack {
-                    Text("""
+                ScrollView(.vertical, showsIndicators: true) {
+                    VStack {
+                        Text("""
                         もう、誰が誰に
                         何円返せばいいんだっけ
                         で迷わない。
                         """)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(20)
-                    
-                    Text("""
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(20)
+                        
+                        Text("""
                         友達と旅行で、レンタカーや高速道路代など、
                         お金の貸し借りをした時、最終的に誰が誰に何
                         円返すべきか混乱することはありませんか？
@@ -32,40 +33,66 @@ struct ContentView: View {
                         さをシンプルに解決してくれる無料のサービス
                         です。
                         """)
-                    .font(.
-                          headline)
-                    .padding(20)
-                    
-                    NavigationLink(destination: CreateGrooupView()) {
-                        Text("はじめる")
-                            .bold()
-                            .padding(.horizontal, 140)
-                            .padding(.vertical, 12)
-                            .foregroundStyle(Color("main"))
-                            .background(Color.white)
-                            .cornerRadius(25)
+                        .font(.
+                              headline)
+                        .padding(20)
+                        
+                        NavigationLink(destination: CreateGrooupView()) {
+                            Text("はじめる")
+                                .bold()
+                                .padding(.horizontal, 140)
+                                .padding(.vertical, 12)
+                                .foregroundStyle(Color("main"))
+                                .background(Color.white)
+                                .cornerRadius(25)
+                        }
+                        
+                        Spacer()
+                        
+                        HStack {
+                            Text("最近のグループ")
+                                .font(.title3)
+                                .padding(.top, 30)
+                                .padding(.bottom, 10)
+                                .padding(.horizontal, 10)
+                            Spacer()
+                        }
+                        ForEach($groups) { $group in
+                            NavigationLink(destination: HomeView(group: $group)
+                            ) {
+                                HStack {
+                                    Text($group.wrappedValue.groupName)
+                                        .font(.headline)
+                                        .foregroundStyle(Color.black)
+                                    Spacer()
+                                    Image(systemName: "chevron.right")
+                                        .foregroundStyle(Color.gray)
+                                }
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(10)
+                                .shadow(color: .gray.opacity(0.2), radius: 2, x: 0, y: 1)
+                                .padding(.horizontal, 10)
+                            }
+                        }
+                        
+                        
                     }
-                    
-                    Spacer()
-                    
-                    Text("最近のグループ")
-                    ForEach(groups) { group in
-                        Text("\(group.groupName)")
+                    .foregroundStyle(Color.white)
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbarBackground(.visible, for: .navigationBar)
+                    .toolbarBackground(Color("main"), for: .navigationBar)
+                    .toolbar {
+                        ToolbarItem(placement: .principal) {
+                            Text("Walican")
+                                .foregroundColor(Color.white)
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                        }
                     }
-                    
-                    
-                }
-                .foregroundStyle(Color.white)
-                .toolbar {
-                    ToolbarItem(placement: .principal) {
-                        Text("Walican")
-                            .foregroundColor(Color.white)
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
+                    .task {
+                        await loadGroups()
                     }
-                }
-                .task {
-                    await loadGroups()
                 }
             }
         }
