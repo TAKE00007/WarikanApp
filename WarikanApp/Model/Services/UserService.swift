0683ca59369ea1ef9472978654d5339a5d3771b5
TAKE00007 <xshun1590@icloud.com>
2025-09-13 15:07:26 +0900

    home画面を修正

--- WarikanApp/Model/Services/UserService.swift
+++ WarikanApp/Model/Services/UserService.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct UserService {
+    private let userRepository = UserRepository()
+    private let billingRepository = BillingRepository()
+    private let billingParticipantRepository = BillingParticipantRepository()
+    
+    func loadUsersWithBillings(groupId: UUID) async throws -> homeData {
+        async let loadUsers = userRepository.fetchUsers(byGroupId: groupId)
+        async let loadBillings = billingRepository.fetchBillings(byGroupId: groupId)
+        async let loadBillingParticipants = billingParticipantRepository.fetchBillingParticipants(byGroupId: groupId)
+        let (users, billings, billingParticipants) = try await (loadUsers, loadBillings, loadBillingParticipants)
+        
+        return homeData(users: users, billings: billings, billingParticipants: billingParticipants)
+    }
+}
+
+struct homeData {
+    let users: [User]
+    let billings: [Billing]
+    let billingParticipants: [BillingParticipant]
+}
