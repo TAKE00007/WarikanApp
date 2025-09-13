e8afd12f9361e7ff82435855352198fde0854402
TAKE00007 <xshun1590@icloud.com>
2025-09-13 14:15:47 +0900

    ファイル移動させた

--- WarikanApp/Model/Entities/BillingParticipant.swift
+++ WarikanApp/Model/Entities/BillingParticipant.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import FirebaseFirestore
 
 class BillingParticipant: Identifiable {
     let billingId: UUID
@@ -19,57 +18,5 @@ class BillingParticipant: Identifiable {
         self.userId = userId
         self.groupId = groupId
         self.isShare = isShare
-    }
-}
-
-class BillingParticipantRepository {
-    private let db = Firestore.firestore()
-    
-    func addBillingParticipant(_ billingParticipant: BillingParticipant) async throws {
-        let documentId = "\(billingParticipant.billingId.uuidString)_\(billingParticipant.userId.uuidString)"
-        
-        try await db.collection("billingParticipants").document(documentId).setData([
-            "billingId": billingParticipant.billingId.uuidString,
-            "userId": billingParticipant.userId.uuidString,
-            "groupId": billingParticipant.groupId.uuidString,
-            "isShare": billingParticipant.isShare
-        ])
-    }
-    
-    func fetchBillingParticipants(byGroupId groupId: UUID) async throws -> [BillingParticipant] {
-        let snapshot = try await db.collection("billingParticipants")
-            .whereField("groupId", isEqualTo: groupId.uuidString)
-            .getDocuments()
-        
-        let billingParticipants: [BillingParticipant] = snapshot.documents.compactMap { doc in
-            let data = doc.data()
-            guard
-                let billingIdString = data["billingId"] as? String,
-                let billingUUID = UUID(uuidString: billingIdString),
-                let userIdString = data["userId"] as? String,
-                let userUUID = UUID(uuidString: userIdString),
-                let groupIdString = data["groupId"] as? String,
-                let groupUUID = UUID(uuidString: groupIdString),
-                let isShare = data["isShare"] as? Bool
-            else {
-                return nil
-            }
-            
-            let billingParticipant = BillingParticipant(
-                billingId: billingUUID,
-                userId: userUUID,
-                groupId: groupUUID,
-                isShare: isShare
-            )
-            return billingParticipant
-        }
-        return billingParticipants
-    }
-    
-    func updateBillingParticipant(_ billingParticipant: BillingParticipant) async throws {
-        let documentId = "(\(billingParticipant.billingId.uuidString)_\(billingParticipant.userId.uuidString))"
-        try await db.collection("billingParticipants").document(documentId).updateData([
-            "isShare": billingParticipant.isShare
-        ])
     }
 }
---
+++ WarikanApp/Model/Services/UserService.swift
@@ -0,0 +1,8 @@
+//
+//  UserService.swift
+//  WarikanApp
+//
+//  Created by 大竹駿 on 2025/09/13.
+//
+
+import Foundation
