b932dfe269ba206ba87cc06f735a31040a1f131a
TAKE00007 <xshun1590@icloud.com>
2025-09-23 18:56:30 +0900

    タブのデザイン修正

---
+++ WarikanApp/View/Billing/KashikariListView.swift
@@ -0,0 +1,18 @@
+//
+//  KashikariListView.swift
+//  WarikanApp
+//
+//  Created by 大竹駿 on 2025/09/23.
+//
+
+import SwiftUI
+
+struct KashikariListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KashikariListView()
+}
