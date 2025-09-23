3037f5bde4c1acab6fb2bdec0f13d176b5baf36c
TAKE00007 <xshun1590@icloud.com>
2025-09-23 21:15:28 +0900

    支出のViewのデザインが完成

--- WarikanApp/View/Billing/ShishutuListView.swift
+++ WarikanApp/View/Billing/ShishutuListView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct ShishutuListView: View {
+    let users: [User] = [
+        User(id: UUID(), groupId: UUID(), userName: "take", isPay: true),
+        User(id: UUID(), groupId: UUID(), userName: "sho", isPay: false)
+    ]
+    let prices: [Int] = [
+        20081,-20081
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack {
+                ForEach(0..<min(users.count, prices.count), id: \.self) { i in
+                    let user = users[i]
+                    let price = prices[i]
+                    
+                    HStack {
+                        Text(user.userName)
+                        Spacer()
+                        Text("\(price)")
+                    }
+                    .padding()
+                    Divider()
+                }
+                
+            }
+            .listStyle(.plain)
+            
+            HStack {
+                Text("グループ支出合計")
+                    .bold()
+                Spacer()
+                Text("¥277919")
+                    .bold()
+            }
+            .padding()
+            
+            Divider()
+        }
     }
 }
 
