afa7b1393959064a6b95844ddf61fb9838b682e3
TAKE00007 <xshun1590@icloud.com>
2025-09-28 09:27:38 +0900

    refactor途中

--- WarikanApp/Model/Services/BillingService.swift
+++ WarikanApp/Model/Services/BillingService.swift
@@ -15,4 +15,48 @@ struct BillingService {
         try await billingRepository.addBilling(billing)
         try await billingParticipantRepository.addBillingParticipants(billingParticipants)
     }
+    
+    private func updateBillingWithParticipants(billing: Billing, billingParticipants: [BillingParticipant]) async {
+        guard !isSaving else { return }
+        guard let userId = userId else {
+            errorMessage = "ユーザーが選択されていません"; return
+        }
+        //trimmingCharacters: 両端の空白を削除する
+        guard !paymentTitle.trimmingCharacters(in: .whitespaces).isEmpty else {
+            errorMessage = "項目名を入力してください"; return
+        }
+        guard let price = Int(paymentText), price >= 0 else {
+            errorMessage = "金額は数値で入力してください"; return
+        }
+        
+        isSaving = true
+        defer { isSaving = false }
+        
+        //billing更新
+        billing.userId = userId
+        billing.priceTitle = paymentTitle
+        billing.paymentPrice = price
+        
+        //billingParticipantを更新
+        let targets: [BillingParticipant] = billingParticipants
+            .filter { $0.billingId == billing.id }
+            .map { part in
+                part.isShare = users.first { $0.id == part.userId }?.isPay ?? true
+                return part
+            }
+        
+        //払う人(billing.userId)が選択されているけどチェックが外れている場合(billingParticipants.isShare=falseを排除
+        if let targetIsShare = billingParticipants
+            .first (where: { $0.userId == billing.userId && $0.billingId == billing.id })?.isShare, targetIsShare == false {
+            errorMessage = "支払い者のチェックが外れています"; return
+            }
+        
+        do {
+            let repo = BillingRepository()
+            try await repo.updateBillingWithParticipants(billing: billing, participants: targets)
+            dismiss()
+        } catch {
+            errorMessage = "更新に失敗しました: \(error.localizedDescription)"
+        }
+    }
 }
